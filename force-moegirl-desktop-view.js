7d418398a93b00563385fd7d0c8a3adb1ce17c6d
qwq233 <qwq233@qwq2333.top>
2021-07-07 22:45:36 +0800

    Add force-moegirl-desktop-view.js
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

---
+++ force-moegirl-desktop-view.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name             强制萌娘百科桌面版
+// @namespace        https://github.com/qwq233/force-moegirl-desktop-view
+// @description      强制萌娘百科桌面版 Google搜出来的全是手机版页面 萌娘那边也不会自动给跳转orz
+// @version          1.0.0
+// @match            *zh.moegirl.org/*
+// @match            *mzh.moegirl.org/*
+// @match            *zh.moegirl.org.cn/*
+// @match            *mzh.moegirl.org.cn/*
+// @author           qwq233 <qwq233@qwq2333.top>
+// @license          AGPL
+// @supportURL       https://github.com/qwq233/force-moegirl-desktop-view/issues
+// @updateURL https://raw.githubusercontent.com/qwq233/force-moegirl-desktop-view/master/force-moegirl-desktop-view.js
+// @contributionURL  https://afdian.net/@gao_cai_sheng
+// ==/UserScript==
+
+(function () {
+    const url_desktop = "zh.moegirl.org";
+    function replace_url(url){
+        if (url.match('mzh.moegirl.org')){
+            url = url.replace('mzh.moegirl.org',url_desktop);
+        }
+        console.log(url)
+        return url;
+    }
+    url = window.location.href;
+    newUrl = replace_url(url);
+    if (url != newUrl) {
+        location.href = newUrl;
+    }
+})();
